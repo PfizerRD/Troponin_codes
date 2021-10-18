044c2324e39d9045c5c65e6449b3be11c4d6201e
pangjctufts <pangjctufts@gmail.com>
2021-10-18 17:19:23 -0400

    backup
    so far so good

--- step3_cardiomyocyte_iPSC_assay_v0.py
+++ step3_cardiomyocyte_iPSC_assay_v0.py
@@ -139,7 +139,7 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
             print(ii)
         prvs = next
         
-    SC_diff = (np.gradient(SC_values_ref[:-20]))
+    SC_diff = (np.gradient(SC_values_ref[:-10]))
 
     SC_diff_max = np.max(SC_diff)
     SC_diff_min = np.min(SC_diff)
@@ -147,26 +147,9 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
     SC_diff_pos, _ = find_peaks(SC_diff, height= SC_diff_max*0.85,distance=30)
     SC_diff_neg, _ = find_peaks(-SC_diff, height= -SC_diff_min*0.85,distance=30)
 
-    SC_inv_height = np.max(1-SC_values_ref[:-20])
-    dist_peak, _ = find_peaks(1-SC_values_ref[:-20], height=  SC_inv_height*0.85,distance=100)
+    SC_inv_height = np.max(1-SC_values_ref[:-10])
+    dist_peak, _ = find_peaks(1-SC_values_ref[:-10], height=  SC_inv_height*0.85,distance=100)
     
-    
-    fig, (ax1,ax2,ax3) = plt.subplots(3,1)
-
-    ax1.plot(1-SC_values_ref[:-20])
-    ax1.plot(dist_peak,1-SC_values_ref[dist_peak],"*")
-    ###ax = plt.subplot(132)
-    ax2.plot(SC_diff)
-    ax3.plot(abs(SC_diff))
-    ax3.plot(SC_diff_pos,abs(SC_diff[SC_diff_pos]),"o")
-    ax3.plot(SC_diff_neg,abs(SC_diff[SC_diff_neg]),"x")
-    ax3.plot(dist_peak,abs(SC_diff[dist_peak]),"*")
-
-    displayFigureName1 = OutputPath+"\\"+videoFileName+"_result1.png"
-    print(displayFigureName1)
-    fig.savefig(displayFigureName1)
-    fig.clf()
-    plt.close(fig)
     
     ###thresh = threshold_otsu(magSum)
     magSum = np.max(magStack,axis=2)
@@ -188,34 +171,34 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
 
     mask_label = label(cellMask9)
     
-    for jj in range(1,2):# just one object
-        mask_region = (mask_label==jj)
-        mask_region_stack = np.repeat(mask_region[:, :, np.newaxis], magStack.shape[2], axis=2)
-        mask_region_size = np.sum(mask_region)
-        print(mask_region_size)
-        ##plt.imshow(mask_region_stack[:,:,200])
-        magStack_mask = np.multiply(magStack, mask_region_stack.astype(int))
-        flow_trace = np.sum(magStack_mask,axis=0)
-        flow_trace = np.sum(flow_trace, axis=0)/mask_region_size
+    ###for jj in range(0,1):# just one object
+    mask_region = (mask_label>0)
+    mask_region_stack = np.repeat(mask_region[:, :, np.newaxis], magStack.shape[2], axis=2)
+    mask_region_size = np.sum(mask_region)
+    print(mask_region_size)
+    ##plt.imshow(mask_region_stack[:,:,200])
+    magStack_mask = np.multiply(magStack, mask_region_stack.astype(int))
+    flow_trace = np.sum(magStack_mask,axis=0)
+    flow_trace = np.sum(flow_trace, axis=0)/mask_region_size
 
     A_list = []
     B_list = []
 
-    half_width1 = 51 # related to sample freqency
+    half_width1 = 7 # related to sample freqency
+    leftBound1 = SC_diff_pos-half_width1
     ###leftBound1 = dist_peak-half_width1
-    leftBound1 = dist_peak-half_width1
-    ###rightBound1 = dist_peak+5
-    rightBound1 = dist_peak
+    rightBound1 = SC_diff_pos+half_width1
+    ###rightBound1 = dist_peak
 
-    half_width2 = 71 # related to sample freqency
-    ###leftBound2 = dist_peak+10
+    half_width2 = 11 # related to sample freqency
+    leftBound2 = SC_diff_neg-half_width2
+    rightBound2 = SC_diff_neg+half_width2
+    ###leftBound2 = dist_peak+20
     ###rightBound2 = dist_peak+half_width2
-    leftBound2 = dist_peak+15
-    rightBound2 = dist_peak+half_width2
 
 
     for ii in range(len(leftBound1)):
-        if leftBound1[ii]<5 or rightBound1[ii]>len(flow_trace)-40:
+        if leftBound1[ii]<5 or rightBound1[ii]>len(flow_trace)-20:
             continue
 
         maxV = np.max(flow_trace[leftBound1[ii]:rightBound1[ii]])
@@ -223,7 +206,7 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
         A_list.append(neg_ind)
 
     for ii in range(len(leftBound2)):
-        if leftBound2[ii]<5 or rightBound2[ii]>len(flow_trace)-40:
+        if leftBound2[ii]<5 or rightBound2[ii]>len(flow_trace)-20:
             continue
 
         maxV = np.max(flow_trace[leftBound2[ii]:rightBound2[ii]])
@@ -236,7 +219,7 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
     print(Bs)
 
     reg_periods = min(len(As),len(Bs),len(SC_diff_pos),len(SC_diff_neg))
-    for mm in range(reg_periods-1):
+    for mm in range(reg_periods):
         ## writer.writerow( ('subFolder','Optical_A_index', 'Optical_B_index', 'OpticalFlow_A_value','OpticalFlow_B_value','Correlation_A_index','Correlation_B_index','Correlation_A_value','Correlation_B_value') )
         writer.writerow((videoFileName,  As[mm],Bs[mm],flow_trace[As[mm]],flow_trace[Bs[mm]],SC_diff_pos[mm], SC_diff_neg[mm],SC_diff[SC_diff_pos[mm]],SC_diff[SC_diff_neg[mm]]))
 
@@ -244,15 +227,15 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
     fout.close()
 
 
-    fig, (ax1,ax2) = plt.subplots(2,1)
-    plt.rcParams['figure.figsize'] = [18, 18]
+    fig, (ax1,ax2,ax3,ax4) = plt.subplots(4,1)
+    plt.rcParams['figure.figsize'] = [28, 32]
             ###plt.subplot(211)
 
     alpha = 0.8
     img_hsv = color.rgb2hsv(frame1)
     color_mask = np.zeros(frame1.shape)
-    color_mask[...,0] = cellMask9.astype(float)*255
-    color_mask[...,1] = cellMask9.astype(float)*255
+    color_mask[...,0] = mask_region.astype(float)*255
+    color_mask[...,1] = mask_region.astype(float)*255
     #color_mask[...,2] = magMask.astype(np.float)*255
     color_mask_hsv = color.rgb2hsv(color_mask)
 
@@ -262,18 +245,28 @@ def iPSC_pipeline(RootPath,OutputPath,subfolder,ds=1):
 
 
     ax1.imshow(img_masked)
-    ax2.plot(flow_trace[1:-10])
-    ax2.plot(As,flow_trace[As], "x")
-    ax2.plot(Bs,flow_trace[Bs], "d")
-    ax2.plot(dist_peak,flow_trace[dist_peak],'*')
-    ax2.plot(SC_diff_pos,flow_trace[SC_diff_pos],'*')
-    ax2.plot(SC_diff_neg,flow_trace[SC_diff_neg],'*')
+    ax2.plot(flow_trace[1:-10],linewidth=2)
+    ax2.plot(As,flow_trace[As], "x",markersize=8)
+    ax2.plot(Bs,flow_trace[Bs], "d", markersize=8)
+    ax2.plot(dist_peak,flow_trace[dist_peak],'*', markersize=8)
+    ax2.plot(SC_diff_pos,flow_trace[SC_diff_pos],'*', markersize=8)
+    ax2.plot(SC_diff_neg,flow_trace[SC_diff_neg],'*', markersize=8)
+
+
+    ax3.plot(1-SC_values_ref[:-10],linewidth=2)
+    ax3.plot(dist_peak,1-SC_values_ref[dist_peak],"*", markersize=8)
+
+    ax4.plot(abs(SC_diff[1:-10]),linewidth=2)
+    ax4.plot(SC_diff_pos,abs(SC_diff[SC_diff_pos]),"o", markersize=8)
+    ax4.plot(SC_diff_neg,abs(SC_diff[SC_diff_neg]),"x", markersize=8)
+    ax4.plot(dist_peak,abs(SC_diff[dist_peak]),"*")
     
-    displayFigureName2 = OutputPath+"\\"+videoFileName+"_result2.png"
+    displayFigureName2 = OutputPath+"\\"+videoFileName+"_result.png"
     print(displayFigureName2)
     fig.savefig(displayFigureName2)
     fig.clf()
     plt.close(fig)
+
     
     
     
@@ -287,7 +280,7 @@ if __name__ == "__main__":
 
     subfolders = list(listdir_nohidden(RootPath))
  
-    cpu_num = 8
+    cpu_num = 12
  
 
     subFolders = sorted(list(listdir_nohidden(RootPath)))
