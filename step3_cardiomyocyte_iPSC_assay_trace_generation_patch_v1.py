f7f01be02b3bdaecc7048d76f3159614ea73c82a
pangjctufts <pangjctufts@gmail.com>
2022-03-16 14:07:50 -0400

    reorged backup

--- step3_cardiomyocyte_iPSC_assay_trace_generation_patch_v1.py
+++ step3_cardiomyocyte_iPSC_assay_trace_generation_patch_v1.py
@@ -82,7 +82,7 @@ def cellSegmentation(frame_ref):
     I_filt = np.multiply(I_filt,valid_region)
     result0 =  I_filt>thresh
     result1 = binary_opening(result0,disk(11))
-    result1 = binary_erosion(result1,disk(15))
+    ###result1 = binary_erosion(result1,disk(15))
     result1 = remove_small_objects(result1,1500)
     mask_label = label(result1)
     return mask_label
@@ -277,10 +277,10 @@ if __name__ == "__main__":
     ds = 2
 
     ###RootPath =   r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Patterned_Dish2'
-    ###OutputPath = r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Patterned_Dish2_output'
+    ###OutputPath = r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Patterned_Dish2_output_50Hz_unshrink'
     
     RootPath =   r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Mattek_Dish_1'
-    OutputPath = r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Mattek_Dish_1_output'
+    OutputPath = r'Z:\pangj05\RDRU_MYBPC3_2022\0221_iCells_Patterned_Dishes_DataSetAnalysis\220221_ICells_Mattek_Dish_1_output_50Hz_unShrink'
 
 
     ###RootPath =   'Z:\\pangj05\\RDRU_MYBPC3_2021\\20211020DataSetAnalysis\\Plate1'
